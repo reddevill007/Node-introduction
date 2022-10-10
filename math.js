b459f89a7aa3d867f6242763c87326b73e37a53a
reddevill007 <shivampandey2900@gmail.com>
2022-10-10 11:43:05 +0530

    export import node js

---
+++ math.js
@@ -0,0 +1,6 @@
+exports.add = (a, b) => a + b;
+exports.subtract = (a, b) => a - b;
+exports.mulitiply = (a, b) => a * b;
+exports.divide = (a, b) => a / b;
+
+// module.exports = { add, subtract, mulitiply, divide };
--- server.js
+++ server.js
@@ -8,19 +8,26 @@ console.log("hello");
 
 // 4) Has common core module
 // 5) CommonJS module instead of ES6 module
+// 6) Missing some JS APIs like fetch
 
 const os = require("os");
 const path = require("path");
+const { add, subtract, mulitiply, divide } = require("./math");
 
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(mulitiply(2, 3));
+console.log(divide(2, 3));
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+// console.log(__dirname);
+// console.log(__filename);
 
-console.log(path.parse(__filename));
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+// console.log(path.parse(__filename));
