f1cc9c0e86b8e755ed3d3fe3d54ac57506959220
reddevill007 <shivampandey2900@gmail.com>
2023-02-14 09:31:12 +0530

    del

--- tut 01/math.js
+++
@@ -1,6 +0,0 @@
-exports.add = (a, b) => a + b;
-exports.subtract = (a, b) => a - b;
-exports.mulitiply = (a, b) => a * b;
-exports.divide = (a, b) => a / b;
-
-// module.exports = { add, subtract, mulitiply, divide };
