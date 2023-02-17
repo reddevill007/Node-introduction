60ff4b70316b1678954f3367faaf37a38f09facb
reddevill007 <shivampandey2900@gmail.com>
2023-02-17 08:17:06 +0530

    custom server

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,8 +1,11 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()}: ${req.url} New Req Recieved\n`;
+  const myURL = url.parse(req.url);
+  console.log(myURL);
   fs.appendFile("log.txt", log, (err, data) => {
     switch (req.url) {
       case "/":
